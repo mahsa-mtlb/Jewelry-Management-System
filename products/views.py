a4021a8d22943f674c014accbf66143ee9d6dfde
mahsa-mtlb <mhsa.mtlb04@gmail.com>
2026-07-28 01:22:08 +0330

    Create initial template structure

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(LoginRequiredMixin, ListView):
+    model = Product
+
+    template_name = "products/product_list.html"
+
+    context_object_name = "products"
+
+    paginate_by = 10
+
+    ordering = ["-created_at"]
